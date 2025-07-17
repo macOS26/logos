57f5d203fee75ade79b10db7131a82fb08a2074e
SuperBox64 <admin@starplayrx.com>
2025-07-16 20:32:22 -0400

    Step 4 Final: Remove ClipperPathOperations folder and fix JoinType dependency

--- logos inkpen.io/Views/RightPanel/ProfessionalOffsetPathSection.swift
+++ logos inkpen.io/Views/RightPanel/ProfessionalOffsetPathSection.swift
@@ -7,6 +7,42 @@
 
 import SwiftUI
 
+// MARK: - Join Type Definition (Local replacement for ClipperPath JoinType)
+
+enum JoinType: CaseIterable {
+    case round
+    case miter
+    case bevel
+    case square
+    
+    var displayName: String {
+        switch self {
+        case .round: return "Round"
+        case .miter: return "Miter"
+        case .bevel: return "Bevel"
+        case .square: return "Square"
+        }
+    }
+    
+    var iconName: String {
+        switch self {
+        case .round: return "circle"
+        case .miter: return "diamond"
+        case .bevel: return "octagon"
+        case .square: return "square"
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .round: return "Rounded corners (smooth curves)"
+        case .miter: return "Sharp pointed corners"
+        case .bevel: return "Flat angled corners"
+        case .square: return "Square corners"
+        }
+    }
+}
+
 // MARK: - Professional Offset Path Section (Adobe Illustrator Standards)
 
 struct ProfessionalOffsetPathSection: View {
