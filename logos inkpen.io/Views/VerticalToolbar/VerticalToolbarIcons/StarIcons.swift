b4fa449750f7fd26c4a850bae998c56b26624c90
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-09-28 04:23:34 -0400

    Remove unused EightPointStarIcon struct
    - EightPointStarIcon was defined but never used
    - Removed dead code from StarIcons.swift

--- logos inkpen.io/Views/VerticalToolbar/VerticalToolbarIcons/StarIcons.swift
+++ logos inkpen.io/Views/VerticalToolbar/VerticalToolbarIcons/StarIcons.swift
@@ -163,32 +163,3 @@ struct SevenPointStarIcon: View {
 }
 
 // 8-Point Star (compass rose style)
-struct EightPointStarIcon: View {
-    let isSelected: Bool
-    
-    var body: some View {
-        Path { path in
-            // Draw using the exact star formula used by the canvas
-            let center = CGPoint(x: 10, y: 10)
-            let outerRadius: CGFloat = 8 + IconStrokeExpand
-            let innerRadius: CGFloat = outerRadius * 0.40
-            let points: Int = 8
-
-            let angleStep = .pi / Double(points)
-            for i in 0..<(points * 2) {
-                let angle = Double(i) * angleStep - .pi / 2
-                let r = (i % 2 == 0) ? outerRadius : innerRadius
-                let x = center.x + cos(angle) * r
-                let y = center.y + sin(angle) * r
-                if i == 0 {
-                    path.move(to: CGPoint(x: x, y: y))
-                } else {
-                    path.addLine(to: CGPoint(x: x, y: y))
-                }
-            }
-            path.closeSubpath()
-        }
-        .stroke(Color.primary, lineWidth: IconStrokeWidth)
-        .frame(width: 20, height: 20)
-    }
-}
