628e409ea91cedde4c9a20e0eb4f0b5d69783535
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-10-17 21:20:01 -0400

    Fix findText to find text inside groups

--- logos inkpen.io/Utilities/Vector/VectorDocument+Helpers.swift
+++ logos inkpen.io/Utilities/Vector/VectorDocument+Helpers.swift
@@ -341,9 +341,7 @@ extension VectorDocument {
 
         for object in unifiedObjects {
             if case .group(let shape) = object.objectType {
-                if let textShape = shape.groupedShapes.first(where: { $0.id == id }),
-                   let childObject = findObject(by: textShape.id),
-                   case .text = childObject.objectType,
+                if let textShape = shape.groupedShapes.first(where: { $0.id == id && $0.typography != nil }),
                    var vectorText = VectorText.from(textShape) {
                     vectorText.layerIndex = object.layerIndex
                     return vectorText
