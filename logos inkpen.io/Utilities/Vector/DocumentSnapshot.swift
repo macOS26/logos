b8d9d2d4dd84f17842d0817b49df2173b63fce2a
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-10-21 09:01:22 -0400

    Add settings to DocumentSnapshot

--- logos inkpen.io/Utilities/Vector/DocumentSnapshot.swift
+++ logos inkpen.io/Utilities/Vector/DocumentSnapshot.swift
@@ -3,9 +3,21 @@ import Foundation
 struct DocumentSnapshot: Equatable, Codable {
     var objects: [UUID: VectorObject]
     var layers: [Layer]  // In stack order
+    var settings: DocumentSettings
+    var colorSwatches: ColorSwatches
+    var gridSettings: GridSettings
 
-    init(objects: [UUID: VectorObject] = [:], layers: [Layer] = []) {
+    init(
+        objects: [UUID: VectorObject] = [:],
+        layers: [Layer] = [],
+        settings: DocumentSettings = DocumentSettings(),
+        colorSwatches: ColorSwatches = .empty,
+        gridSettings: GridSettings = .default
+    ) {
         self.objects = objects
         self.layers = layers
+        self.settings = settings
+        self.colorSwatches = colorSwatches
+        self.gridSettings = gridSettings
     }
 }
