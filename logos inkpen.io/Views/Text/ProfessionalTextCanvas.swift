543d3a3ae6594d19804e1372c81c5a18bf2f7968
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-10-23 14:50:28 -0400

    Show cursor at click position

--- logos inkpen.io/Views/Text/ProfessionalTextCanvas.swift
+++ logos inkpen.io/Views/Text/ProfessionalTextCanvas.swift
@@ -171,15 +171,6 @@ struct ProfessionalTextCanvas: View {
                 }
             }
 
-            // When entering blue mode (editing), select all text
-            if oldState != .blue && textBoxState == .blue {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
-                    if let window = NSApp.keyWindow,
-                       let textView = window.firstResponder as? NSTextView {
-                        textView.selectAll(nil)
-                    }
-                }
-            }
         }
     }
 
