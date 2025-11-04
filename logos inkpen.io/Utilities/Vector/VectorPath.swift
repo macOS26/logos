910af63e94fb604c66b9a3d1f0e0266b75693639
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-11-04 13:35:43 -0500

    Add backward compatible Codable for PathElement

--- logos inkpen.io/Utilities/Vector/VectorPath.swift
+++ logos inkpen.io/Utilities/Vector/VectorPath.swift
@@ -121,7 +121,7 @@ enum AnchorPointType: String, Codable, Hashable {
     case smooth      // 180° tangent curves
 }
 
-enum PathElement: Codable, Hashable {
+enum PathElement: Hashable {
     case move(to: VectorPoint, pointType: AnchorPointType = .corner)
     case line(to: VectorPoint, pointType: AnchorPointType = .corner)
     case curve(to: VectorPoint, control1: VectorPoint, control2: VectorPoint, pointType: AnchorPointType = .smooth)
@@ -163,6 +163,108 @@ enum PathElement: Codable, Hashable {
             self = .quadCurve(to: to, control: c, pointType: type)
         case .close:
             break
+        }
+    }
+}
+
+// MARK: - Codable Implementation (Backward Compatible)
+extension PathElement: Codable {
+    enum CodingKeys: String, CodingKey {
+        case move, line, curve, quadCurve, close
+    }
+
+    enum MoveCodingKeys: String, CodingKey {
+        case to, pointType
+    }
+
+    enum LineCodingKeys: String, CodingKey {
+        case to, pointType
+    }
+
+    enum CurveCodingKeys: String, CodingKey {
+        case to, control1, control2, pointType
+    }
+
+    enum QuadCurveCodingKeys: String, CodingKey {
+        case to, control, pointType
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        switch self {
+        case .move(let to, let pointType):
+            var nested = container.nestedContainer(keyedBy: MoveCodingKeys.self, forKey: .move)
+            try nested.encode(to, forKey: .to)
+            try nested.encode(pointType, forKey: .pointType)
+
+        case .line(let to, let pointType):
+            var nested = container.nestedContainer(keyedBy: LineCodingKeys.self, forKey: .line)
+            try nested.encode(to, forKey: .to)
+            try nested.encode(pointType, forKey: .pointType)
+
+        case .curve(let to, let control1, let control2, let pointType):
+            var nested = container.nestedContainer(keyedBy: CurveCodingKeys.self, forKey: .curve)
+            try nested.encode(to, forKey: .to)
+            try nested.encode(control1, forKey: .control1)
+            try nested.encode(control2, forKey: .control2)
+            try nested.encode(pointType, forKey: .pointType)
+
+        case .quadCurve(let to, let control, let pointType):
+            var nested = container.nestedContainer(keyedBy: QuadCurveCodingKeys.self, forKey: .quadCurve)
+            try nested.encode(to, forKey: .to)
+            try nested.encode(control, forKey: .control)
+            try nested.encode(pointType, forKey: .pointType)
+
+        case .close:
+            _ = container.nestedContainer(keyedBy: MoveCodingKeys.self, forKey: .close)
+        }
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        if container.contains(.move) {
+            let nested = try container.nestedContainer(keyedBy: MoveCodingKeys.self, forKey: .move)
+            let to = try nested.decode(VectorPoint.self, forKey: .to)
+            // Backward compatible: use default if pointType is missing
+            let pointType = try nested.decodeIfPresent(AnchorPointType.self, forKey: .pointType) ?? .corner
+            self = .move(to: to, pointType: pointType)
+
+        } else if container.contains(.line) {
+            let nested = try container.nestedContainer(keyedBy: LineCodingKeys.self, forKey: .line)
+            let to = try nested.decode(VectorPoint.self, forKey: .to)
+            // Backward compatible: use default if pointType is missing
+            let pointType = try nested.decodeIfPresent(AnchorPointType.self, forKey: .pointType) ?? .corner
+            self = .line(to: to, pointType: pointType)
+
+        } else if container.contains(.curve) {
+            let nested = try container.nestedContainer(keyedBy: CurveCodingKeys.self, forKey: .curve)
+            let to = try nested.decode(VectorPoint.self, forKey: .to)
+            let control1 = try nested.decode(VectorPoint.self, forKey: .control1)
+            let control2 = try nested.decode(VectorPoint.self, forKey: .control2)
+            // Backward compatible: use default if pointType is missing
+            let pointType = try nested.decodeIfPresent(AnchorPointType.self, forKey: .pointType) ?? .smooth
+            self = .curve(to: to, control1: control1, control2: control2, pointType: pointType)
+
+        } else if container.contains(.quadCurve) {
+            let nested = try container.nestedContainer(keyedBy: QuadCurveCodingKeys.self, forKey: .quadCurve)
+            let to = try nested.decode(VectorPoint.self, forKey: .to)
+            let control = try nested.decode(VectorPoint.self, forKey: .control)
+            // Backward compatible: use default if pointType is missing
+            let pointType = try nested.decodeIfPresent(AnchorPointType.self, forKey: .pointType) ?? .smooth
+            self = .quadCurve(to: to, control: control, pointType: pointType)
+
+        } else if container.contains(.close) {
+            self = .close
+
+        } else {
+            throw DecodingError.dataCorrupted(
+                DecodingError.Context(
+                    codingPath: decoder.codingPath,
+                    debugDescription: "Unknown PathElement type"
+                )
+            )
         }
     }
 }
