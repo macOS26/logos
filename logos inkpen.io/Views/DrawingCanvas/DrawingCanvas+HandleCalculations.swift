7182ecfdb5c9dc68d56670f97ee048ff2e21f2a3
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-11-04 13:15:48 -0500

    Fix pattern matching in HandleCalculations and MarkerTool

--- logos inkpen.io/Views/DrawingCanvas/DrawingCanvas+HandleCalculations.swift
+++ logos inkpen.io/Views/DrawingCanvas/DrawingCanvas+HandleCalculations.swift
@@ -9,11 +9,11 @@ extension DrawingCanvas {
         }
 
         if draggedHandleID.handleType == .control2 {
-            guard case .curve(let anchorTo, let control1, _) = elements[draggedHandleID.elementIndex] else { return }
+            guard case .curve(let anchorTo, let control1, _, let pointType) = elements[draggedHandleID.elementIndex] else { return }
 
             let anchorPoint = CGPoint(x: anchorTo.x, y: anchorTo.y)
             let nextIndex = draggedHandleID.elementIndex + 1
-            if nextIndex < elements.count, case .curve(let nextTo, let currentOutgoing, let nextControl2) = elements[nextIndex] {
+            if nextIndex < elements.count, case .curve(let nextTo, let currentOutgoing, let nextControl2, let nextPointType) = elements[nextIndex] {
 
                 let oppositeHandle = calculateLinkedHandle(
                     anchorPoint: anchorPoint,
@@ -21,14 +21,14 @@ extension DrawingCanvas {
                     originalOppositeHandle: CGPoint(x: currentOutgoing.x, y: currentOutgoing.y)
                 )
 
-                elements[draggedHandleID.elementIndex] = .curve(to: anchorTo, control1: control1, control2: VectorPoint(newDraggedPosition.x, newDraggedPosition.y))
-                elements[nextIndex] = .curve(to: nextTo, control1: VectorPoint(oppositeHandle.x, oppositeHandle.y), control2: nextControl2)
+                elements[draggedHandleID.elementIndex] = .curve(to: anchorTo, control1: control1, control2: VectorPoint(newDraggedPosition.x, newDraggedPosition.y), pointType: pointType)
+                elements[nextIndex] = .curve(to: nextTo, control1: VectorPoint(oppositeHandle.x, oppositeHandle.y), control2: nextControl2, pointType: nextPointType)
             }
 
         } else if draggedHandleID.handleType == .control1 {
 
             let prevIndex = draggedHandleID.elementIndex - 1
-            if prevIndex >= 0, case .curve(let anchorTo, let prevControl1, let currentIncoming) = elements[prevIndex] {
+            if prevIndex >= 0, case .curve(let anchorTo, let prevControl1, let currentIncoming, let prevPointType) = elements[prevIndex] {
 
                 let anchorPoint = CGPoint(x: anchorTo.x, y: anchorTo.y)
                 let oppositeHandle = calculateLinkedHandle(
@@ -37,9 +37,9 @@ extension DrawingCanvas {
                     originalOppositeHandle: CGPoint(x: currentIncoming.x, y: currentIncoming.y)
                 )
 
-                if case .curve(let currentTo, _, let currentControl2) = elements[draggedHandleID.elementIndex] {
-                    elements[prevIndex] = .curve(to: anchorTo, control1: prevControl1, control2: VectorPoint(oppositeHandle.x, oppositeHandle.y))
-                    elements[draggedHandleID.elementIndex] = .curve(to: currentTo, control1: VectorPoint(newDraggedPosition.x, newDraggedPosition.y), control2: currentControl2)
+                if case .curve(let currentTo, _, let currentControl2, let currentPointType) = elements[draggedHandleID.elementIndex] {
+                    elements[prevIndex] = .curve(to: anchorTo, control1: prevControl1, control2: VectorPoint(oppositeHandle.x, oppositeHandle.y), pointType: prevPointType)
+                    elements[draggedHandleID.elementIndex] = .curve(to: currentTo, control1: VectorPoint(newDraggedPosition.x, newDraggedPosition.y), control2: currentControl2, pointType: currentPointType)
                 }
             }
         }
