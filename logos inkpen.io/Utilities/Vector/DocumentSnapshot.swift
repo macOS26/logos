4375235625e9ec2e2afa3a7f9bf06e24568742bd
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-11-09 15:01:22 -0500

    Add formatVersion 1.0.27 to DocumentSnapshot

--- logos inkpen.io/Utilities/Vector/DocumentSnapshot.swift
+++ logos inkpen.io/Utilities/Vector/DocumentSnapshot.swift
@@ -2,6 +2,7 @@ import Foundation
 
 struct
 DocumentSnapshot: Equatable, Codable {
+    var formatVersion: String = "1.0.27"
     var objects: [UUID: VectorObject]
     var layers: [Layer]  // In stack order
     var settings: DocumentSettings
@@ -9,12 +10,14 @@ DocumentSnapshot: Equatable, Codable {
     var gridSettings: GridSettings
 
     init(
+        formatVersion: String = "1.0.27",
         objects: [UUID: VectorObject] = [:],
         layers: [Layer] = [],
         settings: DocumentSettings = DocumentSettings(),
         colorSwatches: ColorSwatches = .empty,
         gridSettings: GridSettings = .default
     ) {
+        self.formatVersion = formatVersion
         self.objects = objects
         self.layers = layers
         self.settings = settings
