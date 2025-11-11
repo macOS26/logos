ba592c88523cd4f97369ddb2dd8b2c97ce309c6d
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-11-11 17:44:58 -0500

    Add checkerboard tile opacity test

--- logos inkpen.io/Utilities/Metal/ImageTileRenderer.metal
+++ logos inkpen.io/Utilities/Metal/ImageTileRenderer.metal
@@ -4,11 +4,13 @@ using namespace metal;
 struct TileVertex {
     float2 position [[attribute(0)]];
     float2 texCoord [[attribute(1)]];
+    float opacity [[attribute(2)]];
 };
 
 struct TileVertexOut {
     float4 position [[position]];
     float2 texCoord;
+    float opacity;
 };
 
 // Vertex shader - transforms tile quad to screen space
@@ -17,18 +19,18 @@ vertex TileVertexOut tileVertexShader(TileVertex in [[stage_in]],
     TileVertexOut out;
     out.position = mvpMatrix * float4(in.position, 0.0, 1.0);
     out.texCoord = in.texCoord;
+    out.opacity = in.opacity;
     return out;
 }
 
-// Fragment shader - samples texture at tile coordinates
+// Fragment shader - samples texture at tile coordinates with per-tile opacity
 fragment float4 tileFragmentShader(TileVertexOut in [[stage_in]],
-                                   texture2d<float> imageTexture [[texture(0)]],
-                                   constant float& opacity [[buffer(0)]]) {
+                                   texture2d<float> imageTexture [[texture(0)]]) {
     constexpr sampler textureSampler(mag_filter::linear,
                                      min_filter::linear,
                                      address::clamp_to_edge);
 
     float4 color = imageTexture.sample(textureSampler, in.texCoord);
-    color.a *= opacity;
+    color.a *= in.opacity;
     return color;
 }
--- logos inkpen.io/Utilities/Metal/MetalImageTileRenderer.swift
+++ logos inkpen.io/Utilities/Metal/MetalImageTileRenderer.swift
@@ -40,8 +40,12 @@ class MetalImageTileRenderer {
         vertexDescriptor.attributes[1].format = .float2
         vertexDescriptor.attributes[1].offset = MemoryLayout<Float>.size * 2
         vertexDescriptor.attributes[1].bufferIndex = 0
-        // Layout stride (4 floats per vertex: x, y, u, v)
-        vertexDescriptor.layouts[0].stride = MemoryLayout<Float>.size * 4
+        // Opacity: float at attribute(2)
+        vertexDescriptor.attributes[2].format = .float
+        vertexDescriptor.attributes[2].offset = MemoryLayout<Float>.size * 4
+        vertexDescriptor.attributes[2].bufferIndex = 0
+        // Layout stride (5 floats per vertex: x, y, u, v, opacity)
+        vertexDescriptor.layouts[0].stride = MemoryLayout<Float>.size * 5
         vertexDescriptor.layouts[0].stepFunction = .perVertex
 
         pipelineDescriptor.vertexDescriptor = vertexDescriptor
@@ -170,9 +174,6 @@ class MetalImageTileRenderer {
         renderEncoder.setRenderPipelineState(pipelineState)
         renderEncoder.setFragmentTexture(sourceTexture, index: 0)
 
-        var opacityBuffer: Float = 1.0
-        renderEncoder.setFragmentBytes(&opacityBuffer, length: MemoryLayout<Float>.size, index: 0)
-
         // Create orthographic projection matrix for the output size
         var mvpMatrix = createOrthographicMatrix(width: Float(outputSize.width), height: Float(outputSize.height))
         renderEncoder.setVertexBytes(&mvpMatrix, length: MemoryLayout<simd_float4x4>.size, index: 1)
@@ -187,7 +188,7 @@ class MetalImageTileRenderer {
         var vertices: [Float] = []
         var indices: [UInt16] = []
 
-        for (index, (_, tileRect)) in tiles.enumerated() {
+        for (index, (coord, tileRect)) in tiles.enumerated() {
             let baseVertex = UInt16(index * 4)
 
             // Calculate destination rect
@@ -202,12 +203,15 @@ class MetalImageTileRenderer {
             let texMaxX = Float(tileRect.maxX / CGFloat(imageWidth))
             let texMaxY = Float(tileRect.maxY / CGFloat(imageHeight))
 
-            // Quad vertices for this tile (position + texCoord)
+            // Checkerboard opacity: 50% for alternating tiles
+            let tileOpacity: Float = ((coord.x + coord.y) % 2 == 0) ? 0.5 : 1.0
+
+            // Quad vertices for this tile (position + texCoord + opacity)
             vertices.append(contentsOf: [
-                destX,        destY,        texMinX, texMinY,  // Bottom-left
-                destX + destW, destY,        texMaxX, texMinY,  // Bottom-right
-                destX,        destY + destH, texMinX, texMaxY,  // Top-left
-                destX + destW, destY + destH, texMaxX, texMaxY   // Top-right
+                destX,        destY,        texMinX, texMinY, tileOpacity,  // Bottom-left
+                destX + destW, destY,        texMaxX, texMinY, tileOpacity,  // Bottom-right
+                destX,        destY + destH, texMinX, texMaxY, tileOpacity,  // Top-left
+                destX + destW, destY + destH, texMaxX, texMaxY, tileOpacity   // Top-right
             ])
 
             // Two triangles per tile
