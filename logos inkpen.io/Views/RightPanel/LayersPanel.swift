0c556887d3d4880e46fd4a5a56d9892097933d34
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-11-15 22:07:52 -0500

    Remove unused renamingLayerIndex

--- logos inkpen.io/Views/RightPanel/LayersPanel.swift
+++ logos inkpen.io/Views/RightPanel/LayersPanel.swift
@@ -91,7 +91,6 @@ struct LayersPanel: View {
     @Binding var processedLayersDuringDrag: Set<Int>
     @Binding var processedObjectsDuringDrag: Set<UUID>
 
-    @State private var renamingLayerIndex: Int?
     @State private var newLayerName: String = ""
     @State private var showColorPicker: Bool = false
     @State private var overlaysEnabled: Bool = true
