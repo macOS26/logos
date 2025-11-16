4abb146a24edd608a58e829b60408d466d973a27
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-11-16 17:03:15 -0500

    Simplify with PathElement.endpoint

--- logos inkpen.io/Utilities/DrawingCanvas/PointPositionUtility.swift
+++ logos inkpen.io/Utilities/DrawingCanvas/PointPositionUtility.swift
@@ -5,16 +5,7 @@ func getPointPositionExternal(_ pointID: PointID, in document: VectorDocument) -
         if case .shape(let shape) = object.objectType,
            shape.id == pointID.shapeID {
             guard pointID.elementIndex < shape.path.elements.count else { return nil }
-            let element = shape.path.elements[pointID.elementIndex]
-
-            switch element {
-            case .move(let to), .line(let to):
-                return to
-            case .curve(let to, _, _), .quadCurve(let to, _):
-                return to
-            case .close:
-                return nil
-            }
+            return shape.path.elements[pointID.elementIndex].endpoint
         }
     }
     return nil
