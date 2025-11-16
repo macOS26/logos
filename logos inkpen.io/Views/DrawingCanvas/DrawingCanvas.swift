7013ab913a8efb853af1268257ffb70c2659523e
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-11-16 09:35:51 -0500

    Remove unused coincidentPointClusters variable

--- logos inkpen.io/Views/DrawingCanvas/DrawingCanvas.swift
+++ logos inkpen.io/Views/DrawingCanvas/DrawingCanvas.swift
@@ -170,7 +170,6 @@ struct DrawingCanvas: View {
     @State internal var currentMousePosition: CGPoint = .zero
     @State internal var liveCornerRadii: [Double] = []
     @State internal var originalCornerRadii: [Double] = []
-    @State internal var coincidentPointClusters: [HashableCGPoint: [PointID]] = [:]
     @State internal var coincidentPointTolerance: Double = 0.1
     @State internal var isEditingText = false
     @State internal var editingTextID: UUID? = nil
