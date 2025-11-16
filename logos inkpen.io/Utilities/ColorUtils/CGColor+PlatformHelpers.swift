b68a99fc033a4e28983b6e08dbcf713af6ce2425
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-11-16 17:50:18 -0500

    Add CGColor platform helpers

---
+++ logos inkpen.io/Utilities/ColorUtils/CGColor+PlatformHelpers.swift
@@ -0,0 +1,34 @@
+import Foundation
+import CoreGraphics
+
+#if canImport(AppKit)
+import AppKit
+typealias PlatformColor = NSColor
+#elseif canImport(UIKit)
+import UIKit
+typealias PlatformColor = UIColor
+#endif
+
+extension CGColor {
+    /// Converts CGColor to platform-specific color (NSColor on macOS, UIColor on iOS/iPadOS)
+    var platformColor: PlatformColor {
+        #if canImport(AppKit)
+        return NSColor(cgColor: self) ?? NSColor.black
+        #elseif canImport(UIKit)
+        return UIColor(cgColor: self)
+        #endif
+    }
+
+    /// Common color constants as CGColor
+    static var black: CGColor {
+        CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+
+    static var white: CGColor {
+        CGColor(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+
+    static var clear: CGColor {
+        CGColor(red: 0, green: 0, blue: 0, alpha: 0)
+    }
+}
