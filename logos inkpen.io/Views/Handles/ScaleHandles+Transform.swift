b1d1217c0ed36242d4489e7cc5ca6821948e5e50
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-11-16 19:11:52 -0500

    CPU SIMD optimize ScaleHandles scale extraction

--- logos inkpen.io/Views/Handles/ScaleHandles+Transform.swift
+++ logos inkpen.io/Views/Handles/ScaleHandles+Transform.swift
@@ -178,8 +178,9 @@ extension ScaleHandles {
     func applyTransformToCornerRadiiLocal(shape: inout VectorShape, transform: CGAffineTransform) {
         guard !transform.isIdentity else { return }
 
-        let scaleX = sqrt(transform.a * transform.a + transform.c * transform.c)
-        let scaleY = sqrt(transform.b * transform.b + transform.d * transform.d)
+        // SIMD-optimized scale extraction
+        let scaleX = simd_length(SIMD2(transform.a, transform.c))
+        let scaleY = simd_length(SIMD2(transform.b, transform.d))
         let scaleRatio = max(scaleX, scaleY) / min(scaleX, scaleY)
         let maxReasonableRatio: CGFloat = 3.0
 
